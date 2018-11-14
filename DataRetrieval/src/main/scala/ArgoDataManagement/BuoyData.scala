21d9d698a79774ef87c626f99b3b52c6af1c806f
RaimiSol <raimi.solorzano@gmail.com>
2018-11-14 22:45:47 +0100

    Reshaped java netcdf with buoy data to spark dataframe

--- DataRetrieval/src/main/scala/ArgoDataManagement/BuoyData.scala
+++ DataRetrieval/src/main/scala/ArgoDataManagement/BuoyData.scala
@@ -1,45 +1,26 @@
 package ArgoDataManagement
 
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import ucar.nc2._
-
-
 import Util.ArgoFloatException
 import collection.JavaConverters._
 
-class BuoyData ( netcdf_path:String="src/main/resources/1900063_prof.nc") {
+class BuoyData(netcdf_path: String = "src/main/resources/1900063_prof.nc") {
 
-
-
-  def getJavaNetCDFObject:NetcdfFile = NetcdfFile.open(netcdf_path)
+  def getJavaNetCDFObject: NetcdfFile = NetcdfFile.open(netcdf_path)
 
   def getVariables: Seq[Variable] = getJavaNetCDFObject.getVariables().asScala
 
-  def getGlobalAttributes: Map[String,String]={
+  def getGlobalAttributes: Map[String, String] = {
     getJavaNetCDFObject.getGlobalAttributes().asScala
-      .map(globAttr=>(globAttr.getName,globAttr.getStringValue)).toMap
+      .map(globAttr => (globAttr.getName, globAttr.getStringValue)).toMap
   }
 
-
-  def getMap:Map[String,Array[_ >: Double with Int with Char with Float <: AnyVal]] = getVariables.map(file_var => {
-    val key = caseConvert(file_var.getShortName)
-    val value = file_var.read.copyTo1DJavaArray
-    file_var.read.getElementType.toString match {
-      case "double" => (key, value.asInstanceOf[Array[Double]])
-      case "int" => (key, value.asInstanceOf[Array[Int]])
-      case "char" => (key, value.asInstanceOf[Array[Char]])
-      case "float" => (key, value.asInstanceOf[Array[Float]])
-      case _ => throw new ArgoFloatException("\n   --- Required: [int || double || char || float] as variable datatypes from raw netCDF\n"+
-          f"   +++ Found:\n   $file_var.read")
-    }
-  }).toMap
-
-  // TODO In future better with symbol or enum instead of string
-  def getLongitude = getMap("longitude")
-  def getLatitude = getMap("latitude")
-
-
   // MACRO_CASE to camelCase
-  private[this] def caseConvert(macroCaseString:String):String={
+  private[this] def caseConvert(macroCaseString: String): String = {
     val parts = macroCaseString.toLowerCase.split("_")
 
     val h = parts.head
@@ -50,28 +31,106 @@ class BuoyData ( netcdf_path:String="src/main/resources/1900063_prof.nc") {
     val res = h +: t
     return res.mkString("")
   }
-}
+  
+  def getMap: Map[String, Array[_ >: Double with Int with String with Float]] = {
+    getVariables
+      .filter(file_var => file_var.getDimensions.get(0).getName == "N_PROF")
+      .map(file_var => {
+        val key = caseConvert(file_var.getShortName)
+        val dims = file_var.getDimensions.asScala
+        val value = file_var.read.copyTo1DJavaArray
+        file_var.read.getElementType.toString match {
+          case "double" => (key, value.asInstanceOf[Array[Double]])
+          case "int"    => (key, value.asInstanceOf[Array[Int]])
+          case "char" => {
+            if (dims.size > 1) {
+              (key, value.asInstanceOf[Array[Char]].grouped(dims(1).getLength).toArray.map(arrOfChar => arrOfChar.mkString.trim))
+            } else {
+              (key, value.asInstanceOf[Array[Char]].map(arrOfChar => {
+                arrOfChar.toString()
+              }))
 
-case class Buk(pres:Array[Float], date_creation:Array[Char], config_mission_number:Array[Int],
-    scientific_calib_coefficient:Array[Char], temp_adjusted_error:Array[Float],
-    parameter:Array[Char], juld_qc:Array[Char], data_mode:Array[Char],
-    history_institution:Array[Char], pres_adjusted_qc:Array[Char], latitude:Array[Double],
-    profile_pres_qc:Array[Char], psal:Array[Float], profile_psal_qc:Array[Char],
-    positioning_system:Array[Char], format_version:Array[Char], cycle_number:Array[Int],
-    position_qc:Array[Char], vertical_sampling_scheme:Array[Char], psal_adjusted:Array[Float],
-    date_update:Array[Char], scientific_calib_comment:Array[Char], direction:Array[Char],
-    pres_adjusted_error:Array[Float], psal_adjusted_error:Array[Float], data_state_indicator:Array[Char],
-    platform_number:Array[Char], pres_adjusted:Array[Float], longitude:Array[Double],
-    history_stop_pres:Array[Float], juld:Array[Double], temp_adjusted:Array[Float],
-    platform_type:Array[Char], data_type:Array[Char], wmo_inst_type:Array[Char],
-    temp_adjusted_qc:Array[Char], temp:Array[Float], history_action:Array[Char], handbook_version:Array[Char],
-    history_qctest:Array[Char], scientific_calib_equation:Array[Char], history_step:Array[Char],
-    history_parameter:Array[Char], history_software_release:Array[Char], pres_qc:Array[Char],
-    history_previous_value:Array[Float], data_centre:Array[Char], history_date:Array[Char], profile_temp_qc:Array[Char],
-    reference_date_time:Array[Char], scientific_calib_date:Array[Char], firmware_version:Array[Char], dc_reference:Array[Char],
-    pi_name:Array[Char], station_parameters:Array[Char], history_reference:Array[Char], float_serial_no:Array[Char],
-    psal_qc:Array[Char], history_start_pres:Array[Float], project_name:Array[Char], history_software:Array[Char],
-    psal_adjusted_qc:Array[Char], temp_qc:Array[Char], juld_location:Double)
+            }
+          }
+          case "float" => (key, value.asInstanceOf[Array[Float]])
+          case _ => throw new ArgoFloatException("\n   --- Required: [int || double || char || float] as variable datatypes from raw netCDF\n" +
+            f"   +++ Found:\n   $file_var.read")
+        }
+      }).toMap
+  }
+  
+  private[this] def preprocessData = {
+    getVariables.
+    filter(file_var => file_var.getDimensions.get(0).getName == "N_PROF").
+    map(file_var => {
+        val key = caseConvert(file_var.getShortName)
+        val dims = file_var.getDimensions.asScala
+        val value = file_var.read.copyToNDJavaArray
+        file_var.read.getElementType.toString match {
+          case "double" =>  {
+              if (dims.size == 2) {
+              (StructField(key, ArrayType(DoubleType,true), true), value.asInstanceOf[Array[Array[Double]]].toList)
+              }else{
+                 (StructField(key, DoubleType, true), value.asInstanceOf[Array[Double]].toList) 
+              }
+          }
+          case "int"    => {
+              if (dims.size == 2) {
+              (StructField(key, ArrayType(IntegerType,true), true), value.asInstanceOf[Array[Array[Int]]].toList)
+              }else{
+                 (StructField(key, IntegerType, true), value.asInstanceOf[Array[Int]].toList) 
+              }
+          }
+          case "char" => {
+              if (dims.size == 4) {
+              (StructField(key, StringType, true), value.asInstanceOf[Array[Array[Array[Array[Char]]]]].map(arrOfChar => arrOfChar.mkString.trim).toList)
+            } else if (dims.size == 3) {
+              (StructField(key, StringType, true), value.asInstanceOf[Array[Array[Array[Char]]]].map(arrOfChar => arrOfChar.mkString.trim).toList)
+            } else if (dims.size == 2) {
+              (StructField(key, StringType, true), value.asInstanceOf[Array[Array[Char]]].map(arrOfChar => arrOfChar.mkString.trim).toList)
+            } else {
+              (StructField(key, StringType, true), value.asInstanceOf[Array[Char]].map(arrOfChar => {
+                arrOfChar.toString()
+              }).toList)
+
+            }
+          }
+          case "float" =>  {
+              if (dims.size == 2) {
+                  (StructField(key, ArrayType(FloatType,true), true), value.asInstanceOf[Array[Array[Float]]].toList) 
+              }else{
+                 (StructField(key, FloatType, true), value.asInstanceOf[Array[Float]].toList) 
+              }
+          }
+          case _ => throw new ArgoFloatException("\n   --- Required: [int || double || char || float] as variable datatypes from raw netCDF\n" +
+            f"   +++ Found:\n   $file_var.read")
+        }
+      }).toList.
+        map(tuples => List(tuples._1, tuples._2)).
+        transpose
+        
+  }
+  
+  def getRDD(sc: SparkContext):RDD[Row]={
+    val groupedData = preprocessData(1).map(arr => arr.asInstanceOf[List[Any]]).
+        transpose.
+        map(arr => Row(arr:_*))
+    sc.parallelize(groupedData)
+  }
+  
+  private[this] def getSchema:StructType={
+     StructType(preprocessData(0).asInstanceOf[List[StructField]])
+  }
+  
+  def getDF(sc:SparkContext,sqlContext: SQLContext)={
+    sqlContext.createDataFrame(getRDD(sc), getSchema)
+  }
+  // TODO In future better with symbol or enum instead of string
+  def getLongitude = getMap("longitude")
+  def getLatitude = getMap("latitude")
+
+  
+}
 
 
 case class Buoy(longi: Double, lati: Double)
--- DataRetrieval/src/main/scala/main/RunProcedure.scala
+++ DataRetrieval/src/main/scala/main/RunProcedure.scala
@@ -1,20 +1,39 @@
 package main
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
+
 import database.DB
 import ArgoDataManagement.BuoyData
 
-
 object RunProcedure {
 
+  // Omit INFO log in console
+  val rootLogger = Logger.getLogger("org").setLevel(Level.WARN)
+
+  val conf = new SparkConf()
+    .setMaster("local")
+    .setAppName("HTW-Argo")
+  val sc = new SparkContext(conf)
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL for Argo Data")
+    .config(conf)
+    .getOrCreate()
+
   def main(args: Array[String]) {
-    dbDemo
+    //dbDemo
+    buoyDataDemo
   }
-  
+
   def dbDemo: Unit = {
     println("-------- START : mongodb demo ---------")
     val testDB = new DB
     val buoys = new BuoyData
-    //testDB.insertFirstBuoy(buoys)
+    testDB.insertFirstBuoy(buoys)
     testDB.insertAllBuoys(buoys)
     testDB.close
     println("-------- END : mongodb demo ---------")
@@ -25,6 +44,8 @@ object RunProcedure {
     val bd = new BuoyData
     println(s"Longitude array:\n[${bd.getLongitude.mkString(",")}]")
     println(bd.getGlobalAttributes)
+    println(bd.getMap.keys)
+    println(bd.getDF(sc, spark.sqlContext).show())
     println("-------- END : Buoy data demo ---------")
   }
 
