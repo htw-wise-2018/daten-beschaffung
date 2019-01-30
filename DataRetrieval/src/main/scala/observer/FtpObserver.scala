c4276fd91aef55211f209380f58e72dd9810bee3
RaimiSol <raimi.solorzano@gmail.com>
2019-01-30 20:54:36 +0100

    Added scalaDoc to ftpobserver

--- DataRetrieval/src/main/scala/observer/FtpObserver.scala
+++ DataRetrieval/src/main/scala/observer/FtpObserver.scala
@@ -11,6 +11,14 @@ import scala.concurrent.Await
 import scala.concurrent.Future
 import akka.actor.ActorLogging
 
+/** Companion Object as recommended practice for Akka actors 
+  * 
+  * props is a factory method to construct the ftpobserver object
+  * 
+  * @author Raimi Solorzano Niederhausen - s0557978@htw-berlin.de Contact
+  * @see See [[https://doc.akka.io/docs/akka/2.5.5/scala/actors.html#recommended-practices]] for more
+  * information on Akka's recommended practices for actors 
+  */
 object FtpObserver {
   def props(callback: ()=>Unit) = Props(new FtpObserver(callback))
 }
@@ -18,6 +26,17 @@ case object Start
 case object TimerKey
 case object Polling
 case object Stop
+/** Class to check for modifications at the ftp server permanently in request intervals.
+  *
+  * @constructor Creates a ftp observer object.
+  * @param callback Is a function that is called by the actor when relevant modifications were detected at the ftp 
+  * @param server Ftp server to observe
+  * @param port Ftp port (default ftp port 21)
+  * @param user Ftp user (no user necessary)
+  * @param password Ftp password (no password necessary)
+  * @author Raimi Solorzano Niederhausen - s0557978@htw-berlin.de Contact
+  * @see See [[https://github.com/htw-wise-2018]] for more information.
+  */
 class FtpObserver (
   val callback: () => Unit,
   val server:   String = "ftp.ifremer.fr",
@@ -25,24 +44,30 @@ class FtpObserver (
   val user:     String = "anonymous",
   val password: String = "password") extends Actor with ActorLogging with Timers{
 
+  /** Akka timer to start the observation task 
+    * TimerKey serves as unique id for the timer
+    * Start is the signal to send to initialize the Akka actor
+    * 0 second is the initial delay
+    */
   timers.startSingleTimer(TimerKey, Start, 0 second)
   
+  // Method to receive messages for the actor
   override def receive: Receive = {
     case Start => {
       log.info("Start ftp-polling")
-      self ! Polling // First poll
-      timers.startPeriodicTimer(TimerKey, Polling, 1 day) // Periodic polling
+      self ! Polling // Actor sends message to itself. Start first poll.
+      timers.startPeriodicTimer(TimerKey, Polling, 1 day) // Periodic polling. Sends 'Polling' message every day to the actor
     }
-    case Polling => {
+    case Polling => { // Check modification of specific file at ftp server
       log.info("Poll")
       val currentModificationTime = getModificationTime()
       log.info(s"Current modification time: ${currentModificationTime}")
-        if (lastModificationTime != currentModificationTime) {
+        if (lastModificationTime != currentModificationTime) { // if file modified
           lastModificationTime = currentModificationTime
           callback()
         }
     }
-    case Stop => {
+    case Stop => { // Stop actor 
       log.warning("Stopping")
       timers.cancel(TimerKey)
       context.stop(self)
@@ -50,8 +75,11 @@ class FtpObserver (
       
   }
   
-  var lastModificationTime = ""
+  var lastModificationTime = "" // saves last modification time to compare with the new modification time
   
+  /** Check modification of specific file at ftp server
+    * @param filePath Path of file to observe. Path relative to defined ftp server.
+    */
   @throws(classOf[Exception])
   def getModificationTime(filePath: String = "/ifremer/argo/ar_index_this_week_prof.txt"): String = {
     val ftp = new FTPClient()
